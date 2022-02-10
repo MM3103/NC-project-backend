5a8edce4fa9e3ffbac9a74a433ca3e6a15741124
MM3103 <misha.makhalin@yandex.ru>
2022-02-10 17:40:40 +0300

    commit

--- src/main/java/my/pr/controller/OrderController.java
+++ src/main/java/my/pr/controller/OrderController.java
@@ -11,7 +11,6 @@ import org.springdoc.api.OpenApiResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.mail.MessagingException;
 import java.util.HashMap;
 import java.util.List;
